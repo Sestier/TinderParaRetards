b950f6d6bd80f1676ea4408a04ce796e52d1ded2
Maestro <Maestro@imac-de-maestro-5.local>
2018-01-29 18:48:36 -0700

    primeravance

--- transiciones2/ViewController.swift
+++ transiciones2/ViewController.swift
@@ -9,6 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var vwContenedor: UIView!
+    @IBOutlet weak var btnSi: UIButton!
+    @IBOutlet weak var btnNo: UIButton!
+    
+    
+    @IBAction func doValueChangeSwitch(_ sender: Any) {
+        UIView.transition(with: vwContenedor, duration: 0.75, options: [.transitionFlipFromRight], animations: {
+        }, completion: nil)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        btnSi.layer.cornerRadius  = 40
+        btnNo.layer.cornerRadius  = 40
+        
+    }
+    
+    @IBAction func btnSiAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func btnNoAction(_ sender: Any) {
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
